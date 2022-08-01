27e603526e5939bb79cefacab4443d1881523e9f
Arne Munthe-Kaas <45269747+h593267@users.noreply.github.com>
2022-08-01 12:38:30 +0200

    entity

---
+++ src/main/java/no/arnemunthekaas/db/TRDAO.java
@@ -0,0 +1,4 @@
+package no.arnemunthekaas.db;
+
+public class TRDAO {
+}
--- src/main/java/no/arnemunthekaas/db/TemperatureReading.java
+++ src/main/java/no/arnemunthekaas/db/TemperatureReading.java
@@ -1,8 +1,52 @@
 package no.arnemunthekaas.db;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+import java.time.Instant;
 
 
 @Entity
 public class TemperatureReading {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ID;
+    private Timestamp timestamp;
+    private float indoorTemp;
+    private float outdoorTemp;
+
+    public TemperatureReading () {};
+
+    /**
+     * For use without timestamp (gets current timestamp from system time)
+     *
+     * @param indoorTemp
+     * @param outdoorTemp
+     */
+    public TemperatureReading (float indoorTemp, float outdoorTemp) {
+        this.indoorTemp = indoorTemp;
+        this.outdoorTemp = outdoorTemp;
+
+        this.timestamp = Timestamp.from(Instant.now());
+    }
+
+    /**
+     * For use with timestamp
+     *
+     * See https://playground.arduino.cc/Code/DateTime/ and https://www.postgresql.org/docs/current/datatype-datetime.html#DATATYPE-DATETIME-INPUT
+     *
+     * @param indoorTemp
+     * @param outdoorTemp
+     * @param seconds
+     **/
+    public TemperatureReading (float indoorTemp, float outdoorTemp, int seconds) {
+        this.indoorTemp = indoorTemp;
+        this.outdoorTemp = outdoorTemp;
+
+        // Convert seconds to ms
+        this.timestamp = new Timestamp((long) seconds * 1000);
+    }
 }
